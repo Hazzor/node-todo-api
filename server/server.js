57c83b238359b58921748f90fa6a3b24f979e21c
Hazzor <hesterliong@gmail.com>
2017-07-17 21:23:46 +0800

    Add test for GET /todos

--- server/server.js
+++ server/server.js
@@ -29,16 +29,16 @@ app.post('/todos', (req,res)=> {
 
 app.get('/todos', (req, res)=> {
     Todo.find().then((todos)=>{
-        
+
         res.send({
             todos : todos,
             code : 200
-        })
+        });
     }, (e)=>{
 
         res.status(400).send('Unable to save todo');
     });
-})
+});
 
 app.listen(3000, ()=> {
     console.log('Started on port 3000');
