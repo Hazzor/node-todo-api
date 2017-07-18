24de0a456917d3764d9d339ff6ed1ced3baa261c
Hazzor <hesterliong@gmail.com>
2017-07-18 21:52:00 +0800

    Setup separate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,10 @@ var mongoose = require('mongoose');
 // use native promise
 mongoose.Promise = global.Promise;
 //persistent connection, mongoose is using callback by default
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose : mongoose
 }
+
+// process.env.NODE_ENV = development,test,production
